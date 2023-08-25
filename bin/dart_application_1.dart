0b7abc092b049b9948001d2930d569c573910862
shahd-alrahbi <138566899+shahd-alrahbi@users.noreply.github.com>
2023-08-25 22:04:14 +0400

    Initial commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,20 @@
+void main() {
+  // print(123);
+  // print("Dart Course");
+  // print('Dart Course');
+  // print(true);
+  // print(null);
+  // print(12.5);
+  // !-----------------------------
+
+  int x = 12;
+  String s = "Saad";
+  double d = 12.5;
+  String c = 'c';
+
+  print(x);
+  print(d);
+  print(c);
+  print(s[0] + " " + s[1] + " " + s[2] + " " + s[3]);
+  
+}
